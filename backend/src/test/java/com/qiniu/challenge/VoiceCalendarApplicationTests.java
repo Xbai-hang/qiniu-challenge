413f602ad2a9c0b2ea8f284ab50e4f8e77541b13
皖兎氺缶 <11373268+wanxi-fou@user.noreply.gitee.com>
2026-05-29 17:19:21 +0800

    feat: 初始化后端脚手架

---
+++ backend/src/test/java/com/qiniu/challenge/VoiceCalendarApplicationTests.java
@@ -0,0 +1,12 @@
+package com.qiniu.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
